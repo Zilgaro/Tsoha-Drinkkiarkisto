96a6c78708d18e2669646ab860cf709a09497924
Zilgaro <verner_saaristo@msn.com>
2017-01-21 10:58:57 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="Vonsaari"
+USERNAME="vonsaari"
 # korvaa tämä haluamallasi kansion nimellä
 PROJECT_FOLDER="Drinkkilista"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
