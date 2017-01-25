a957e1fda80a583522a30aab0c92671a72957f5c
Zilgaro <verner_saaristo@msn.com>
2017-01-25 19:25:58 +0200

    Sivusuunnitelmat tehty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Client(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+);
+
+CREATE TABLE Ingredient (
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	amount float NOT NULL,
+);
+
+CREATE TABLE Drink(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	glass varchar(50),
+	drink_type varchar(50),
+	ingredient integer NOT NULL,
+	description varchar(500),
+	FOREIGN KEY(ingredient) REFERENCES Ingredient(id),
+);
+
+CREATE TABLE Rating(
+	
+	client integer NOT NULL,
+	drink integer NOT NULL,
+	FOREIGN KEY(client) REFERENCES Client(id),
+	FOREIGN KEY(drink) REFERENCES Drink(id),
+);
