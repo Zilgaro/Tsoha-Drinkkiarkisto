e4cb9591210ad780e3a4044e309dc7b6beff2049
Zilgaro <verner_saaristo@msn.com>
2017-01-26 17:29:13 +0200

    Dokumentaatio mallillaan ja routet testisivuihin

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -9,6 +9,22 @@
 
     public static function sandbox(){
       // Testaa koodiasi täällä
+      View::make('game_show.html');
+    }
+
+    public static function drink_list() {
+      View::make('drink_list.html');
+    }
+
+    public static function drink_show() {
       View::make('drink_show.html');
+    }
+
+    public static function drink_edit() {
+      View::make('drink_show.html');
+    }
+
+    public static function login() {
+      View::make('login.html');
     }
   }
