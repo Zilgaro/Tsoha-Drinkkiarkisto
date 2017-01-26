2177239c33f7bfa0c41d5fb75ec7e8a8ef6717df
Zilgaro <verner_saaristo@msn.com>
2017-01-26 18:40:32 +0200

    Tokan viikon palautus toimii kokonaisuudessaan, oon aika ylpee ittestäni

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,34 +1,34 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
-CREATE TABLE Client(
+CREATE TABLE Client (
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	password varchar(50) NOT NULL
 );
 
 CREATE TABLE Ingredient (
-	name varchar(50) NOT NULL,
-	description Text,
+	name varchar(50) PRIMARY KEY NOT NULL,
+	description Text
 );
 
-CREATE TABLE Drink(
+CREATE TABLE Drink (
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	glass varchar(50),
 	drink_type varchar(50),
-	description Text,
+	description Text
 );
 
-CREATE TABLE Recipe(
+CREATE TABLE Recipe (
 	drink_id integer NOT NULL,
 	ingredient varchar(50) NOT NULL,
 	FOREIGN KEY(drink_id) REFERENCES Drink(id),
-	FOREIGN KEY(ingredient) REFERENCES Ingredient(name),
+	FOREIGN KEY(ingredient) REFERENCES Ingredient(name)
 );
 
-CREATE TABLE Rating(
+CREATE TABLE Rating ( 	
 	client integer NOT NULL,
 	drink integer NOT NULL,
 	rating float NOT NULL,
 	FOREIGN KEY(client) REFERENCES Client(id),
-	FOREIGN KEY(drink) REFERENCES Drink(id),
+	FOREIGN KEY(drink) REFERENCES Drink(id)
 );
